f3bfc7c7f260ad0a3b033d327bdff291244b6eba
kmallavarapu <kmallavarapu@MC90.corp.miraclesoft.com>
2021-05-06 18:37:04 +0530

    created district api
    Signed-off-by: kmallavarapu <kmallavarapu@miraclesoft.com>

--- CovidProject/src/main/java/com/covid/india/CovidController.java
+++ CovidProject/src/main/java/com/covid/india/CovidController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +26,38 @@ public class CovidController {
 				System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
 		}
 		return result;
+	}
+
+	@GetMapping("/covid19/district/{statename}")
+	@CrossOrigin
+	public List getCovid19DistrictDataByStateName(@PathVariable("statename") String stateName) {
+		Map response = new HashMap();
+		List finalData = new ArrayList();
+		Map districtDataMap = null;
+		String uri = "https://api.covid19india.org/state_district_wise.json";
+		RestTemplate restTemplate = new RestTemplate();
+		Map<Object, Object> result = (Map<Object, Object>) restTemplate.getForObject(uri, Map.class);
+		Map<String, Object> districtMap = null;
+		for (Map.Entry<Object, Object> entry : result.entrySet()) {
+			if (!entry.getKey().toString().equalsIgnoreCase("State Unassigned"))
+				if (entry.getKey().toString().equalsIgnoreCase(stateName)) {
+					districtMap = (Map<String, Object>) entry.getValue();
+					Map<String, Object> data = (Map<String, Object>) districtMap.get("districtData");
+					for (Map.Entry<String, Object> entryDistrict : data.entrySet()) {
+						Map finalMap = (Map) data.get(entryDistrict.getKey());
+						districtDataMap = new HashMap();
+						districtDataMap.put("active", finalMap.get("active"));
+						districtDataMap.put("confirmed", finalMap.get("confirmed"));
+						districtDataMap.put("deceased", finalMap.get("deceased"));
+						districtDataMap.put("recovered", finalMap.get("recovered"));
+						districtDataMap.put("district", entryDistrict.getKey());
+						finalData.add(districtDataMap);
+					}
+					break;
+				}
+		}
+		response.put("data", finalData);
+		return finalData;
 	}
 
 	@GetMapping("/covid19/india")
